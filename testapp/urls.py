583a1dc1fa934604fe16b18ab46a1e23c2bf6536
Amal Santhosh <amalsanthosh1403@gmail.com>
2025-02-05 21:35:49 +0530

    urls updated

--- testapp/urls.py
+++ testapp/urls.py
@@ -4,11 +4,11 @@ from .import views
 urlpatterns = [
     path('', views.index, name='index'),
 
-    path('login/', views.login_view, name='login'),
-    path('logout/', views.logout_view, name='logout'),
+    path('api/login/', views.login_view, name='login'),
+    path('api/logout/', views.logout_view, name='logout'),
 
-    path('register/', views.register_participant, name='register'),
-    path('verify_qr_code/', views.verify_participant, name='verify_qr_code'),
+    path('api/register/', views.register_participant, name='register'),
+    path('api/verify_qr_code/', views.verify_participant, name='verify_qr_code'),
 
     path('api/verify_face/', views.verify_face_api, name='verify_face_api'),
     path('verify_face/', views.verify_face, name='verify_face')
